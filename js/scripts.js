c0a097c71d5286ff36d3bba322c12c325f79c9ff
ART89-prog <dykpgilkom@mail.ru>
2023-12-11 21:56:28 +0300

    6753675754

---
+++ js/scripts.js
@@ -0,0 +1,180 @@
+$(() => {
+
+
+	// Ширина окна для ресайза
+	WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+	WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+	BODY = document.getElementsByTagName('body')[0]
+	OVERLAY = document.querySelector('.overlay')
+
+
+	$("form").submit(function() {
+        // Получение ID формы
+        var formID = $(this).attr('id');
+        var formNm = document.getElementById(formID);
+        $.ajax({
+            type: "POST",
+            url: 'send.php',
+            data: new FormData(formNm),
+            processData: false,
+            contentType: false,
+            beforeSend: function() {
+                // Вывод текста в процессе отправки
+                //$(formNm).html('<p style="text-align:center">Отправка...</p>');
+            },
+            success: function(data) {
+                // Вывод текста результата отправки
+                //$(formNm).html('<p style="text-align:center">' + data + '</p>');
+
+
+               /* if(formID=="feedback-form-4")
+                {
+                    ym(82348417,'reachGoal','FORM1')
+                }*/
+                Fancybox.close()
+                Fancybox.show([{
+                    src: "#modal",
+                    type: 'inline'
+                }])
+                $('form').trigger("reset");              
+
+            },
+            error: function(jqXHR, text, error) {
+                // Вывод текста ошибки отправки
+                //$(formNm).html(error);
+            }
+        });
+        return false;
+    });
+
+
+	// Моб. меню
+	$('header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').addClass('active')
+		$('body').addClass('menu_open')
+		$('header .menu').addClass('show')
+		$('.overlay').fadeIn(300)
+	})
+
+	$('header .close_btn, header .menu .item a, .overlay').click((e) => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').removeClass('active')
+		$('body').removeClass('menu_open')
+		$('header .menu').removeClass('show')
+		$('.overlay').fadeOut(300)
+	})
+
+
+	$('body').on('click', '.modal_link', function (e) {
+		e.preventDefault()
+
+		Fancybox.close(true)
+		Fancybox.show([{
+			src: $(this).data('content'),
+			type: 'inline',
+		}]);
+	})
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.trapFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.placeFocusBack = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+	Fancybox.defaults.template = {
+		closeButton: '<img src=images/close.svg>',
+		// spinner: '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="25 25 50 50" tabindex="-1"><circle cx="50" cy="50" r="20"/></svg>',
+		// main: null
+	}
+
+	// Показать контент 
+	$(".link-more").click(function (e) {
+		e.preventDefault();
+		$(".program_item").removeClass("hide");
+		$(".link-more").addClass("active");
+	});
+
+
+
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
+
+
+	// Аккордион
+	$('body').on('click', '.accordion .accordion_item .head', function (e) {
+		e.preventDefault()
+
+		const $item = $(this).closest('.accordion_item'),
+			$accordion = $(this).closest('.accordion')
+
+		if ($item.hasClass('active')) {
+			$item.removeClass('active').find('.data').slideUp(400)
+		} else {
+			$accordion.find('.accordion_item').removeClass('active')
+			$accordion.find('.data').slideUp(400)
+
+			$item.addClass('active').find('.data').slideDown(400)
+		}
+	})
+
+
+
+	// Скрол к пунктам меню
+	$(".scroll").on("click", function (e) {
+		e.preventDefault();
+		let id = $(this).attr("href");
+
+		$("html, body").animate({
+			scrollTop: $(id).offset().top - 40
+		}, {
+			duration: 400,
+			easing: "swing"
+		});
+	});
+
+
+
+
+	window.addEventListener('resize', function () {
+		WH = window.innerHeight || document.clientHeight || document.getElementsByTagName('body')[0].clientHeight
+
+		let windowW = window.outerWidth
+
+		if (typeof WW !== 'undefined' && WW != windowW) {
+
+
+			// Перезапись ширины окна
+			WW = window.innerWidth || document.clientWidth || document.getElementsByTagName('body')[0].clientWidth
+
+
+			// Моб. версия
+			if (!fakeResize) {
+				fakeResize = true
+				fakeResize2 = false
+
+				document.getElementsByTagName('meta')['viewport'].content = 'width=device-width, initial-scale=1, maximum-scale=1'
+			}
+
+			if (!fakeResize2) {
+				fakeResize2 = true
+
+				if (windowW < 375) document.getElementsByTagName('meta')['viewport'].content = 'width=375, user-scalable=no'
+			} else {
+				fakeResize = false
+				fakeResize2 = true
+			}
+		}
+	})
+
+
+
+})
